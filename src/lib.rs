63786d1c2289d2782c1948a3f67490487760254c
Maarten van Gompel <proycon@anaproy.nl>
2020-09-05 21:38:30 +0200

    further implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,32 @@
 extern crate rand;
 extern crate chrono;
+extern crate ansi_term;
 
 use std::fs::File;
 use std::io::{Write,Read,BufReader,BufRead,Error,ErrorKind};
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::collections::{HashMap,HashSet};
+use std::fmt;
 use rand::prelude::{thread_rng,Rng};
 use chrono::NaiveDateTime;
+use ansi_term::Colour;
 
+pub struct VocaSession {
+    columns: Vec<String>,
+    decks: Vec<String>,
+    ///interval in minutes
+    intervals: Vec<u32>,
+    returntofirst: bool,
+    filename: String,
+    ///Configuration of columns to show for each side of the card
+    showcolumns: Vec<Vec<u8>>,
+    ///list delimiter
+    listdelimiter: Option<String>,
+}
 
 pub struct VocaData {
-    header: Vec<String>,
+    session: VocaSession,
     cards: Vec<VocaCard>,
-    decks: Vec<String>,
-    //interval in minutes
-    intervals: Vec<u32>,
 }
 
 pub struct VocaCard {
@@ -22,36 +35,71 @@ pub struct VocaCard {
     deck: u8
 }
 
+#[derive(Debug,Copy,Clone)]
+pub enum PrintFormat {
+    Plain,
+    AnsiColour
+}
+
 impl VocaData {
     pub fn from_file(filename: &str) -> Result<Self,std::io::Error> {
         let file = File::open(filename)?;
         let reader = BufReader::new(file);
         let mut cards: Vec<VocaCard> = Vec::new();
-        let mut header: Vec<String> = Vec::new();
+        let mut columns: Vec<String> = Vec::new();
         let mut decks: Vec<String> = Vec::new();
         let mut intervals: Vec<u32> = Vec::new();
-        for (i, line) in reader.lines().enumerate() {
+        let mut showcolumns: Vec<Vec<u8>> = Vec::new();
+        let mut listdelimiter: Option<String> = None;
+        let mut columncount: u8 = 0;
+        let mut returntofirst = false;
+        for line in reader.lines() {
             let line = line?;
-            if i == 0 && !line.is_empty() && !line.contains("deck#") && !line.contains("due@") {
-                //header
-                header = VocaCard::parse_line(&line)?.fields;
-            }
             if line.starts_with('#') {
                 //metadata or comment
                 if line.starts_with("#decks:") {
-                    decks = line[7..].trim().split(",").map(|s| s.to_owned()).collect();
+                    decks = line[7..].trim().split(",").map(|s| s.trim().to_owned()).collect();
                 } else if line.starts_with("#intervals:") {
                     intervals = line[11..].trim().split(",").map(|s| s.parse::<u32>().expect("parsing interval")).collect();
+                } else if line.starts_with("#columns:") {
+                    columns = line[9..].trim().split(",").map(|s| s.trim().to_owned()).collect();
+                } else if line.starts_with("#showcolumns:") {
+                    //may be specified multiple times for multiple 'sides' of the card
+                    showcolumns.push(line[13..].trim().split(",").map( |s|
+                        columns.iter().enumerate() .find(|&r| r.1 == s.trim() )
+                        .expect(format!("showcolumns references a non-existing column: {}",s).as_str())
+                        .0
+                    ).map(|n| n as u8 ).collect());
+                } else if line.starts_with("#listdelimiter:") {
+                    listdelimiter = Some(line[15..].trim().to_owned());
+                } else if line == "#returntofirst" {
+                    returntofirst = true;
                 }
             } else if !line.is_empty() {
-                cards.push(VocaCard::parse_line(&line)?);
+                let card = VocaCard::parse_line(&line)?;
+                let length = card.fields.len() as u8;
+                if length > columncount {
+                   columncount = length;
+                }
+                cards.push(card);
             }
         }
+        if showcolumns.is_empty() {
+            //default configuration: two sides
+            showcolumns.push(vec!(0)); //first column on front side
+            showcolumns.push((1..columncount).collect()); //other columns on back side
+        }
         Ok(VocaData {
-            header: header,
             cards: cards,
-            decks: decks,
-            intervals: intervals,
+            session: VocaSession {
+                columns: columns,
+                decks: decks,
+                intervals: intervals,
+                returntofirst: returntofirst,
+                filename: filename.to_owned(),
+                showcolumns: showcolumns,
+                listdelimiter: listdelimiter
+            }
         })
     }
 
@@ -88,6 +136,47 @@ impl VocaData {
             return Some(&mut self.cards[choice]);
         }
         None
+    }
+
+
+    pub fn write(&self) -> Result<(),std::io::Error> {
+        let mut file = std::fs::File::create(self.session.filename.as_str())?;
+        //metadata first
+        if !self.session.decks.is_empty() {
+            file.write(b"#decks: ")?;
+            file.write(self.session.decks.join(",").as_bytes() )?;
+            file.write(b"\n")?;
+        }
+        if !self.session.intervals.is_empty() {
+            file.write(b"#intervals: ")?;
+            file.write(self.session.intervals.iter().map(|s| format!("{}",s)).collect::<Vec<String>>().join(",").as_bytes() )?;
+            file.write(b"\n")?;
+        }
+        if let Some(listdelimiter) = &self.session.listdelimiter {
+            file.write(b"#listdelimiter: ")?;
+            file.write(listdelimiter.as_bytes())?;
+            file.write(b"\n")?;
+        }
+        if self.session.returntofirst {
+            file.write(b"#returntofirst\n")?;
+        }
+        if !self.session.columns.is_empty() {
+            file.write(b"#columns: ")?;
+            file.write(self.session.columns.join(",").as_bytes() )?;
+            file.write(b"\n")?;
+            for showcolumns in self.session.showcolumns.iter() {
+                file.write(b"#showcolumns: ")?;
+                file.write(showcolumns.iter().map(|n| format!("{}", self.session.columns[*n as usize]).to_string()).collect::<Vec<String>>().join(",").as_bytes() )?;
+                file.write(b"\n")?;
+            }
+        }
+        file.write(b"#---\n")?; //end of metadata
+        //contents
+        for card in self.cards.iter() {
+            file.write(card.write_to_string().as_bytes())?;
+            file.write(b"\n")?;
+        }
+        Ok(())
     }
 }
 
@@ -137,7 +226,7 @@ impl VocaCard {
         })
     }
 
-    pub fn output(&self) -> String {
+    pub fn write_to_string(&self) -> String {
         let mut result: String = String::new();
         for (i, field) in self.fields.iter().enumerate() {
             if i > 0 {
@@ -156,6 +245,72 @@ impl VocaCard {
         result
     }
 
-    pub fn move_to_deck(&mut self, deck: u8, intervals: &Vec<u32>)  {
+    pub fn move_to_deck(&mut self, deck: u8, session: &VocaSession)  {
+        if let Some(interval) = session.intervals.get(deck as usize) {
+            self.due = Some(NaiveDateTime::from_timestamp(
+                SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get time").as_secs() as i64 + (interval * 60) as i64, 0
+            ));
+        }
+        self.deck = deck;
     }
+
+    pub fn correct(&mut self, session: &VocaSession) {
+        if ((self.deck+1) as usize) < session.decks.len() {
+            self.move_to_deck(self.deck+1, session);
+        } else {
+            self.move_to_deck(self.deck, session);
+        }
+    }
+
+    pub fn incorrect(&mut self, session: &VocaSession) {
+        if self.deck > 0 && !session.returntofirst {
+            self.move_to_deck(self.deck-1, session);
+        } else {
+            self.move_to_deck(0, session);
+        }
+    }
+
+    pub fn print_to_string(&self, side: u8, session: &VocaSession, format: PrintFormat, wraplist: bool) -> Result<String, std::fmt::Error> {
+        if let Some(showcolumns) = session.showcolumns.get(side as usize) {
+            let mut output: String = String::new();
+            for showcolumn in showcolumns.iter() {
+                let field = self.print_field_to_string(*showcolumn, session, format, wraplist)?;
+                output += field.as_str();
+                output += "\n";
+            }
+            Ok(output)
+        } else {
+            Err(fmt::Error)
+        }
+    }
+
+    pub fn print_field_to_string(&self, index: u8, session: &VocaSession, format: PrintFormat, wraplist: bool) -> Result<String, std::fmt::Error> {
+        if let Some(field) = self.fields.get(index as usize) {
+            let output = if let Some(listdelimiter) = &session.listdelimiter {
+                if wraplist {
+                    field.replace(listdelimiter.as_str(), "\n")
+                } else {
+                    field.clone() //maybe TODO: use Cow instead of cloning for better performance
+                }
+            } else {
+                field.clone()
+            };
+            match format {
+                PrintFormat::Plain => Ok(output),
+                PrintFormat::AnsiColour => {
+                    match index {
+                        0 => Ok(Colour::Green.paint(output).to_string()),
+                        1 => Ok(Colour::Cyan.paint(output).to_string()),
+                        2 => Ok(Colour::Yellow.paint(output).to_string()),
+                        3 => Ok(Colour::Purple.paint(output).to_string()),
+                        4 => Ok(Colour::Blue.paint(output).to_string()),
+                        _ => Ok(output),
+                    }
+                }
+            }
+        } else {
+            Ok(String::new()) //empty string
+        }
+    }
+
 }
